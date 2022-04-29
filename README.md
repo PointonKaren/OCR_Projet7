f79a8ddbf3dcd794f706fbab1183d6a79968f2fe
Karen Pointon <karen.pointon.webdev@gmail.com>
2022-04-29 11:25:18 +0200

    Mise en place du backend

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Projet 7 de la formation Développement Web d'OpenClassrooms
 
+## Frontend
+
+## Backend
+
+### Packages à installer :
+
+* bcrypt
+* e-mail validator
+* express
+* jsonwebtoken
+* multer
+* nodemon
+* password-validator
+  
+
+
 *Readme à compléter*
---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,39 @@
+const multer = require("multer");
+const path = require("path");
+
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+}; // Dictionnaire des extensions acceptées
+
+/**
+ * Configuration du chemin et du nom à appliquer au fichier entrant
+ */
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.replace(" ", "_");
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, `${name}_${Date.now()}.${extension}`);
+  },
+});
+
+/**
+ * Ajout du fichier entrant à la BDD
+ */
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, callback) => {
+    const ext = path.extname(file.originalname).toLowerCase();
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+      // Quelle que soit la casse, seules les extensions png, jpg et jpeg seront acceptées.
+      return callback(null, false);
+    }
+    callback(null, true);
+  },
+});
+
+module.exports = upload.single("image");
