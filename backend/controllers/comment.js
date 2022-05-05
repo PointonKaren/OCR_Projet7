ce99263a3b00e9f525a3d8f08ca1abc583fbac64
Karen Pointon <karen.pointon.webdev@gmail.com>
2022-05-05 17:59:20 +0200

    Création squelette controller comment.js

---
+++ backend/controllers/comment.js
@@ -0,0 +1,33 @@
+const Comment = require("../models/Comment");
+
+/**
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+exports.createComment = (req, res, next) => {};
+
+/**
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+exports.getOneComment = (req, res, next) => {};
+
+/**
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+exports.deleteComment = (req, res, next) => {};
+
+/**
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+exports.modifyComment = (req, res, next) => {};
