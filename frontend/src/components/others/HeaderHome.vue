50b3bdde5f0ce755f938ccc66a648eab445eaadb
Karen Pointon <karen.pointon.webdev@gmail.com>
2022-06-08 18:19:56 +0200

    Optimisation css headers

--- frontend/src/components/others/HeaderHome.vue
+++ frontend/src/components/others/HeaderHome.vue
@@ -1,63 +1,21 @@
 <template>
   <!--Header-->
   <header>
-    <div id="header">
-      <img :src="require('@/assets/logo.png')" alt="Logo de Groupomania" />
-      <div id="header__buttons">
-        <ProfileButton @click="toggle" />
-        <a href="mailto:admin@groupomania.com" title="contact"
-          ><button aria-label="Contact" class="button contact">Contact</button>
-          <button aria-label="Contact" class="button contact__icon">
-            <i class="fa-regular fa-envelope"></i>
-          </button>
-        </a>
-      </div>
+    <img :src="require('@/assets/logo.png')" alt="Logo de Groupomania" />
+    <div id="header__buttons">
+      <a href="mailto:admin@groupomania.com" title="contact"
+        ><button aria-label="Contact" class="button contact">Contact</button>
+        <button aria-label="Contact" class="button contact__icon">
+          <i class="fa-regular fa-envelope"></i>
+        </button>
+      </a>
     </div>
-    <!-- Apparition progressive du profil au clic sur le bouton -->
-    <Transition>
-      <div v-if="profile_is_here">
-        <ProfileCard />
-      </div>
-    </Transition>
   </header>
 </template>
 
 <script>
-import ProfileButton from "../user/ProfileButton.vue";
-import ProfileCard from "../user/ProfileCard.vue";
-import { mapState } from "vuex";
-
 export default {
-  name: "HeaderLogged",
-
-  components: {
-    ProfileButton,
-    ProfileCard,
-  },
-
-  computed: {
-    ...mapState({
-      user: "userInfos",
-    }),
-  },
-
-  /**
-   * Par défaut, le profil utilisateur n'est pas visible
-   */
-  data() {
-    return {
-      profile_is_here: false,
-    };
-  },
-
-  /**
-   * Bascule le profil de visible à non visible
-   */
-  methods: {
-    toggle() {
-      this.profile_is_here = !this.profile_is_here;
-    },
-  },
+  name: "HeaderHome",
 };
 </script>
 
@@ -66,41 +24,16 @@ export default {
 @import "./scss/_mixins.scss";
 @import "./scss/_buttons.scss";
 
-#header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 10px 20px;
-  border-bottom: 3px solid $tertiaire;
-  background-color: lightgrey;
-  img {
-    margin-left: 20px;
-  }
+header {
+  @include header;
 }
 
 @media screen and (max-width: 1200px) {
-  #header {
-    padding-left: 0;
-    padding-right: 0;
-
-    img {
-      width: 50vw;
-      max-width: 200px;
-    }
+  header {
+    @include header_responsive;
 
     #header__buttons {
-      width: 30vw;
       max-width: 50px;
-
-      a {
-        .contact {
-          display: none;
-        }
-
-        .contact__icon {
-          display: inline;
-        }
-      }
     }
   }
 }
